a01a94d552dfdf54ec9d3dcb247dc3cfc6459ace
Carl J. Mosca <carljmosca@gmail.com>
2018-11-12 12:28:51 -0500

    logging

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+LOG_LOCATION="/var/log"    
+exec >> $LOG_LOCATION/wordpress-install.log 2>&1
+
 yum install wget epel-release yum-utils -y
 yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 yum-config-manager --enable remi-php72
@@ -20,9 +23,9 @@ cd /tmp && \
     rm wordpress-social-login.zip && \
     mv wordpress-social-login /var/www/html/wp-content/plugins/
 
-cp wp-config.php /var/www/html/    
+cp ./wp-config.php /var/www/html/    
 
-cp servername.conf /etc/httpd/conf.d/
+cp ./servername.conf /etc/httpd/conf.d/
 
 sed -i "s|SSLCertificateFile /etc/pki/tls/certs/localhost.crt|SSLCertificateFile /etc/pki/tls/certs/server.crt|g" /etc/httpd/conf.d/ssl.conf && \
 sed -i "s|SSLCertificateKeyFile /etc/pki/tls/private/localhost.key|SSLCertificateKeyFile /etc/pki/tls/private/server.key|g" /etc/httpd/conf.d/ssl.conf
